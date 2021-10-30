5eaad9f7d7dfb6bf40ea4249fdb5ac1bf6283188
StormFlaate <fflaate@hotmail.no>
2021-10-30 19:51:17 +0200

    Pushing local changes

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -6,10 +6,9 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+
+    poly_list = np.ones(degree)
+    for index, j in enumerate(poly_list):
+        poly_list[index] = x**index
+
+    return poly_list
